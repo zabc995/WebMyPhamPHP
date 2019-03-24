ee03578e755e78cc24901b7e001d844fb1a98c35
Son <ngocson.itlonely@gmail.com>
2019-03-24 15:18:43 +0700

    <Feat>: Display product to Home <Docs>: Upload some file support PJ

--- WebMyPham/app/Http/Controllers/PageController.php
+++ WebMyPham/app/Http/Controllers/PageController.php
@@ -16,6 +16,16 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     //
+    public function getIndex(){
+        $slide = Slide::all();
+        //return view('page.trangchu',['slide'=>$slide]);
+        $new_product = Product::where('new',1)->paginate(8);
+        $sanpham_khuyenmai = Product::where('promotion_price','<>',0)->paginate(8);
+
+        //dd($new_product);
+        return view('page.trangchu',compact('slide','new_product','sanpham_khuyenmai'));
+    }
+    
     public function getLienHe(){
         return view('page.lienhe');
     }
